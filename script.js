284856264301b1a819f44d75f3f0b3ae75fa3f1c
domwin <ndominic56@gmail.com>
2024-01-26 18:41:37 -0500

    display option buttons

--- script.js
+++ script.js
@@ -12,9 +12,23 @@ const canvas = document.getElementById("canvas");
 const resultText = document.getElementById("result-text");
 
 //Options values for buttons
-
 let options = {
     colors:["Red", "Blue", "Yellow" ,"Green"],
     shapes:["Square", "Triangle", "Circle", "Rectangle" ],
     languages:["English", "Spanish", "Japanese", "Chinese"],
 };
+
+//count
+let winCount = 0;
+let count = 0;
+let chosenWord = "";
+
+//Display option buttons
+const displayOptions = () => {
+    optionsContainer.innerHTML += `<h3>Please Select An Option</h3>`;
+    let buttonCon = document.createElement("div");
+    for (let value in options) {
+      buttonCon.innerHTML += `<button class="options" onclick="generateWord('${value}')">${value}</button>`;
+    }
+    optionsContainer.appendChild(buttonCon);
+  };

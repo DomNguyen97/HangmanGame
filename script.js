1c1694014065ead18c620ae628ecc1af7e611cf5
domwin <ndominic56@gmail.com>
2024-01-26 18:07:18 -0500

    Inital reference and options

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-* {
-    padding: 0;
-    margin: 0;
-    box-sizing: border-box;
-    font-family: "Poppins", sans-serif;
-  }
+//"Red", "Blue", "Yellow" ,"Green"
+//"Square", Triangle, "Circle", "Rectangle" 
+//"English", "Spanish", "Japanese", "Chinese"  
+
+//Initial References
+const letterContainer = document.getElementById("letter-container");
+const optionsContainer = document.getElementById("options-container");
+const userInputSection = document.getElementById("user-input-section");
+const newGameContainer = document.getElementById("new-game-container");
+const newGameButton = document.getElementById("new-game-button");
+const canvas = document.getElementById("canvas");
+const resultText = document.getElementById("result-text");
+
+//Options values for buttons
+
+let options = {
+    colors:["Red", "Blue", "Yellow" ,"Green"],
+    shapes:["Square", "Triangle", "Circle", "Rectangle" ],
+    languages:["English", "Spanish", "Japanese", "Chinese"],
+};
